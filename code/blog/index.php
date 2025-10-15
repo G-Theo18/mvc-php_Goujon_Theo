b3e083b66aa46ddd2144d66b851c711bba86e838
tg853309 <tgou58@gmail.com>
2025-10-15 18:55:06 +0200

    Isolez l'accès aux données

--- code/blog/index.php
+++ code/blog/index.php
@@ -1,27 +1,9 @@
 <?php
-try {
-   $database = new PDO(
-      'mysql:host=localhost;dbname=blog;charset=utf8',
-      'root',
-      ''
-   );
-} catch (Exception $e) {
-   die('Erreur : ' . $e->getMessage());
-}
 
-$statement = $database->query(
-   "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
-);
+require('src/model.php');
 
-$posts = [];
-while (($row = $statement->fetch())) {
-   $post = [
-      'title' => $row['titre'],
-      'french_creation_date' => $row['date_creation_fr'],
-      'content' => $row['contenu'],
-   ];
-   $posts[] = $post;
-}
+$posts = getPosts();
 
 require('templates/homepage.php');
+
 ?>
