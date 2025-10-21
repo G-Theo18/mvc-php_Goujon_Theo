c82351594d24d15f57e34316be73be213af96db9
tg853309 <tgou58@gmail.com>
2025-10-21 09:50:19 +0200

    Mettez à jour le modèle

--- code/blog/src/model.php
+++ code/blog/src/model.php
@@ -1,26 +1,87 @@
 <?php
 
-function getPosts() {
+// Connexion à la base de données
+function dbConnect() {
     try {
-        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog', 'password');
-    } catch(Exception $e) {
-        die('Erreur : '.$e->getMessage());
+        $database = new PDO(
+            'mysql:host=localhost;dbname=blog;charset=utf8',
+            'blog',
+            'password'
+        );
+        return $database;
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
     }
+}
+
+function getPosts() {
+    $database = dbConnect();
 
     $statement = $database->query(
-        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
+        "SELECT id, title, content, 
+        DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
+        FROM posts 
+        ORDER BY creation_date DESC 
+        LIMIT 0, 5"
     );
+
     $posts = [];
-    while (($row = $statement->fetch())) {
-        $post = [
+    while ($row = $statement->fetch()) {
+        $posts[] = [
+            'identifier' => $row['id'],
             'title' => $row['title'],
             'french_creation_date' => $row['french_creation_date'],
             'content' => $row['content'],
-            'identifier' => $row['id'],
         ];
-
-        $posts[] = $post;
     }
 
     return $posts;
+}
+
+function getPost($identifier) {
+    $database = dbConnect();
+
+    $statement = $database->prepare(
+        "SELECT id, title, content,
+        DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
+        FROM posts 
+        WHERE id = ?"
+    );
+    $statement->execute([$identifier]);
+
+    $row = $statement->fetch();
+    if (!$row) {
+        return null;
+    }
+
+    return [
+        'identifier' => $row['id'],
+        'title' => $row['title'],
+        'french_creation_date' => $row['french_creation_date'],
+        'content' => $row['content'],
+    ];
+}
+
+function getComments($identifier) {
+    $database = dbConnect();
+
+    $statement = $database->prepare(
+        "SELECT id, author, comment,
+        DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
+        FROM comments 
+        WHERE post_id = ? 
+        ORDER BY comment_date DESC"
+    );
+    $statement->execute([$identifier]);
+
+    $comments = [];
+    while ($row = $statement->fetch()) {
+        $comments[] = [
+            'author' => $row['author'],
+            'french_creation_date' => $row['french_creation_date'],
+            'comment' => $row['comment'],
+        ];
+    }
+
+    return $comments;
 }
