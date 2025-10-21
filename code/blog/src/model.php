cdfec42f254a8a115112de9740a161972a41effd
tg853309 <tgou58@gmail.com>
2025-10-21 08:33:44 +0200

    Base pour afficher les commentaires

--- code/blog/src/model.php
+++ code/blog/src/model.php
@@ -1,35 +1,26 @@
 <?php
+
 function getPosts() {
-    // We connect to the database.
     try {
-        $database = new PDO(
-            'mysql:host=localhost;dbname=blog;charset=utf8',
-            'blog',
-            'password'
-        );
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog', 'password');
     } catch(Exception $e) {
         die('Erreur : '.$e->getMessage());
     }
 
-    // We retrieve the 5 last blog posts.
     $statement = $database->query(
-        "SELECT id, titre, contenu, DATE_FORMAT(date_creation,
-        '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr 
-        FROM billets
-        ORDER BY date_creation DESC 
-        LIMIT 0, 5"
+        "SELECT id, titre AS title, contenu AS content, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY date_creation DESC LIMIT 0, 5"
     );
 
     $posts = [];
     while (($row = $statement->fetch())) {
         $post = [
-            'title' => $row['titre'],
-            'french_creation_date' => $row['date_creation_fr'],
-            'content' => $row['contenu'],
+            'title' => $row['title'],
+            'french_creation_date' => $row['french_creation_date'],
+            'content' => $row['content'],
         ];
+
         $posts[] = $post;
     }
 
     return $posts;
 }
-
