1020b78dea4d65217a72a53cc1a8d1e8858eea82
tg853309 <tgou58@gmail.com>
2025-10-21 09:43:05 +0200

    Dynamisez le contrôleur

--- code/blog/post.php
+++ code/blog/post.php
@@ -19,3 +19,14 @@ $comments = [
 ];
 
 require('templates/post.php');
+require('src/model.php');
+
+if (isset($_GET['id']) && $_GET['id'] > 0) {
+    $identifier = $_GET['id'];
+} else {
+    echo 'Erreur : aucun identifiant de billet envoyé';
+    die;
+}
+
+$post = getPost($identifier);
+$comments = getComments($identifier);
